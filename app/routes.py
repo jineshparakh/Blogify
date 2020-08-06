8a52d731e53bb46ab32f87923771c1ad63c190b9
jineshparakh <jineshparakh@hotmail.com>
2020-08-06 18:42:58 +0530

    Update routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
+import os
 from flask import render_template, flash, redirect, request,url_for
 from app import app,db,bcrypt
 from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user,login_required
+import secrets
+from PIL import Image
 
 
 
@@ -76,12 +79,28 @@ def logout():
     logout_user()
     return redirect('home')
 
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _,f_ext=os.path.splitext(form_picture.filename)
+    picture_filename=random_hex+f_ext
+    picture_path=os.path.join(app.root_path, 'static/profile_pics', picture_filename)
+    output_size=(125,125)
+    i=Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_filename
+
+
 
 @app.route('/account', methods=['POST','GET'])
 @login_required
 def account():
     form=UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file=picture_file
+
         current_user.username=form.username.data
         current_user.email=form.email.data
         db.session.commit()
