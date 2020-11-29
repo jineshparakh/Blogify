1fd55503e1f24cd75720f3f4a264373b031a39ad
LethalPants <hatimmurtuza10@gmail.com>
2020-11-29 15:27:46 +0300

    parse markdown and store as HTML

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, url_for, flash, redirect, request, abort
 from flask_login import current_user, login_required
+from werkzeug.utils import html
 from app import db
 from app.models import Post
 from app.posts.forms import PostForm
-
+import markdown
 
 posts=Blueprint('posts', __name__)
 
@@ -13,7 +14,9 @@ posts=Blueprint('posts', __name__)
 def new_post():
     form=PostForm()
     if form.validate_on_submit():
-        post=Post(title=form.title.data, content=form.content.data, author=current_user)
+
+        print(markdown.markdown(form.content.data).replace('\n', ''))
+        post=Post(title=form.title.data, content=markdown.markdown(form.content.data).replace('\n', ''), author=current_user)
         db.session.add(post)
         db.session.commit()
         flash(f'Your Post has been created!!', category='success')
@@ -21,13 +24,13 @@ def new_post():
     return render_template('create_post.html', title='New Post',legend='New Post', form=form)
 
 
-@posts.route('/post/<int:post_id>')  
+@posts.route('/post/<int:post_id>')
 def post(post_id):
-    post=Post.query.get_or_404(post_id)  
+    post=Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
 
-@posts.route('/post/<int:post_id>/update', methods=['GET', 'POST'])  
+@posts.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post=Post.query.get_or_404(post_id)
@@ -40,13 +43,13 @@ def update_post(post_id):
         db.session.commit()
         flash(f'Your Post has been updated!!', category='success')
         return redirect(url_for('posts.post', post_id=post.id))
-    elif request.method=='GET':    
+    elif request.method=='GET':
         form.title.data=post.title
         form.content.data=post.content
     return render_template('create_post.html', title='Update Post', legend='Update Post', form=form)
 
 
-@posts.route('/post/<int:post_id>/delete', methods=['POST'])  
+@posts.route('/post/<int:post_id>/delete', methods=['POST'])
 @login_required
 def delete_post(post_id):
     post=Post.query.get_or_404(post_id)
